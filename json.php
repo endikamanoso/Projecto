72e15bb92937a172a4b53ef942123e36fdc3120a
endikamanoso <triliton@hotmail.com>
2018-05-12 18:28:59 +0200

    HE PUESTO COMENTARIOS DEL ARCHIVO JSON.PHP Y EN ESTE MOMENTO HE CONSEGUIDO QUE EL PORTAL ACTUALICE EL ESTADO DE LAS PLAZAS DEL PARKING.

--- json.php
+++ json.php
@@ -1,27 +1,27 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json');       //CABECERA DEL JSON
 
-$mysqli= new mysqli('localhost','parking','parking','parking');
+$mysqli= new mysqli('localhost','parking','parking','parking'); //CONSULTA
 $query="select id_plaza,modo_plaza
                 from parking.plazas
                 WHERE id_parking=1";
 
-$resultado=$mysqli->query($query);
-echo '{"plazas":{"plaza":[';
-$fila=$resultado->fetch_assoc();
-while ($fila){
-    $id_plaza=$fila["id_plaza"];
-    $modo_plaza=$fila["modo_plaza"];
-    echo '{"id_plaza":"'.$id_plaza.'","modo_plaza":"'.$modo_plaza.'"}';
-    $fila=$resultado->fetch_assoc();
-    if ($fila){
-        echo ',';
+$resultado=$mysqli->query($query);  //LAS FILAS DE LA CONSULTA LAS GUARDA EN $resultado
+echo '{"plazas":{"plaza":[';        //CREO FILA NUEVA EN EL JSON
+$fila=$resultado->fetch_assoc();    //METE LA PRIMERA FILA EN $fila
+while ($fila){                      //MIENTRAS TENGA VALOR $fila, REALIZARÁ EL BUCLE
+    $id_plaza=$fila["id_plaza"];    //LOS VALORES DE ID_PLAZA Y MODO_PLAZA
+    $modo_plaza=$fila["modo_plaza"];//DE ESA FILA FILA LOS METO EN VARIABLES
+    echo '{"id_plaza":"'.$id_plaza.'","modo_plaza":"'.$modo_plaza.'"}'; //METO VALORES EN LA FILA DEL JSON
+    $fila=$resultado->fetch_assoc();    //METO LA SIGUIENTE FILA
+    if ($fila){                         //SI TIENE VALOR $fila, INTRODUCE
+        echo ',';                       //EN EL JSON UNA COMA PARA METER LA SIGUIENTE PLAZA
     }
 }
 
-echo ']';
-echo '}';
-echo '}';
+echo ']';                               //SI NO TIENE VALOR $fila, CIERRO EL CONTINENTE DE LAS PLAZAS CON ']'
+echo '}';                               //
+echo '}';                               //CIERRO EL JSON
 
 
 ?>
